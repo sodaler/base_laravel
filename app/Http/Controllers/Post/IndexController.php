45e81a325d893559f646a9bb07325213c7726f75
sodaler <misha_lector87@mail.ru>
2023-04-05 13:29:06 +0400

    Filter

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\PostFilter;
+use App\Http\Requests\Post\FilterRequest;
 use App\Models\Post;
 
 /**
@@ -11,9 +13,13 @@ use App\Models\Post;
 class IndexController extends BaseController
 {
     // Вызывается при вызове объекта, как функцию
-    public function __invoke()
+    public function __invoke(FilterRequest $request)
     {
-        $posts = Post::paginate(10);
+        $data = $request->validated();
+
+        $filter = app()->make(PostFilter::class, ['queryParams' => array_filter($data)]);
+        $posts = Post::filter($filter)->paginate(10);
+
         return view('post.index', compact('posts'));
     }
 }
